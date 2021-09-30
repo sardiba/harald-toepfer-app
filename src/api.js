4d661267c0c2825e50ccd8d5dcb5b868501e2cf4
Sarah Diba Anissa <sarahdiba29@gmail.com>
2021-09-30 23:31:46 +0200

    first export finish

--- src/api.js
+++ src/api.js
@@ -1,8 +1,16 @@
-export const getPokemonList = async ({ limit = 20 } = {}) => {
-  // TODO: fetch pokemon list
-  return [];
+export const getToepferList = async () => {
+  // TODO: fetch harry potter list
+  const harryPotter = await fetch("http://hp-api.herokuapp.com/api/characters");
+  const characterList = await harryPotter.json();
+  console.log(characterList);
+  return characterList;
 };
 
-export const getPokemon = async ({ name } = {}) => {
-  // TODO: fetch pokemon by name
+export const getToepfer = async ({ name } = {}) => {
+  const character = await getToepferList();
+  const findName = name;
+  const characterName = character.find((theName) => {
+    return theName.name.value === findName ?? "no such character";
+  });
+  console.log();
 };
